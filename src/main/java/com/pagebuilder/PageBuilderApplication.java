68e3155cff4c241204bba12d7bd9301639bea60c
dalvisanket <dalvisanket2010@gmail.com>
2023-10-26 21:01:07 -0400

    #1 : Project Initialization

---
+++ src/main/java/com/pagebuilder/PageBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.pagebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagebuilder/PageBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
